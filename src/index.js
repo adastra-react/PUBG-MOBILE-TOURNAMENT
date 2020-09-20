3d98aa2b93cf9a7f1ade694fea4842f9b8adfdfa
sunielengland <sunielengland22@gmail.com>
2020-09-19 21:14:47 -0500

    pubg mobile tournament V2

--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as firebase from 'firebase'
 import * as serviceWorker from './serviceWorker';
+const config = {
+  apiKey: "AIzaSyBYs6KJdu4wkKZIk0h9muX6etbYBIrqcQk",
+  authDomain: "pubg-mobile-tournament-3bcc1.firebaseapp.com",
+  databaseURL: "https://pubg-mobile-tournament-3bcc1.firebaseio.com",
+  projectId: "pubg-mobile-tournament-3bcc1",
+  storageBucket: "pubg-mobile-tournament-3bcc1.appspot.com",
+  messagingSenderId: "201085907530",
+  appId: "1:201085907530:web:f261c376382bff7df2acb3",
+  measurementId: "G-N6F66ETW65"
+};
 
+firebase.initializeApp(config)
 ReactDOM.render(
   <React.StrictMode>
     <App />
